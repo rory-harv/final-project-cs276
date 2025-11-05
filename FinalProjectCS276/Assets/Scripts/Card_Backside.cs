db536d0bb7a1b7dea532fd017cab1775ec471f13
rory <victoria.harvey1@icloud.com>
2025-11-05 12:56:01 -0800

    editing conditionals

--- FinalProjectCS276/Assets/Scripts/Card_Backside.cs
+++ FinalProjectCS276/Assets/Scripts/Card_Backside.cs
@@ -1,11 +1,31 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Card_Backside : MonoBehaviour
 {
+    public GameObject FrontSide_SquarePrefab;
+    public GameObject Card1_Prefab;
+    public GameObject Card2_Prefab;
+    public GameObject Card3_Prefab;
+    public GameObject Card4_Prefab;
+    public GameObject Card5_Prefab;
+    public GameObject Card6_Prefab;
+    public GameObject Card7_Prefab;
+    public GameObject Card8_Prefab;
+    public GameObject Card9_Prefab;
+    public GameObject Card10_Prefab;
+    public List<string> cardFrontList;
+    public List<GameObject> cardList;
+    public int clicks = 0;
+        
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        cardList = new List<GameObject> {Card1_Prefab, Card2_Prefab, Card3_Prefab, Card4_Prefab,
+        Card5_Prefab, Card6_Prefab, Card7_Prefab, Card8_Prefab, Card9_Prefab, Card10_Prefab};
 
+        cardFrontList = new List<string> { "CardSquare", "CardSquare", "CardCircle", "CardTriangle",
+    "CardOval", "CardCircle", "CardPolygon", "CardOval", "CardTriangle", "CardPolygon"};
     }
 
     // Update is called once per frame
@@ -16,46 +36,59 @@ public class Card_Backside : MonoBehaviour
 
     void Randomize()
     {
-        // start with list of potential cards (5)
-        // randomize card objects to associate with a specific front side card (list of 10 containing 2 duplicates per card type)
-        // in mousedown, take assigned frontside, delete gameobject, and respawn frontside associated card
-
-        // list[backside cards]
-        // list[frontside cards] == [type1, type1, type2, type2 for # of backside cards]
-        // list[randint 1-len(list[backside cards])]
-        // for i in range(len(list[backside cards]))
-            // 
-    
+        //List<string> cardFrontList = new List<string> { "CardSquare", "CardSquare", "Cherry" };
+        //List<GameObject> cardList = new List<GameObject> {Card1_Prefab, Card2_Prefab, Card3_Prefab, Card4_Prefab,
+        //Card5_Prefab, Card6_Prefab, Card7_Prefab, Card8_Prefab, Card9_Prefab, Card10_Prefab};
     }
+
 
     void OnMouseDown()
     {
-        objectName = gameObject.name;
-        if (objectName == "Card_BackSide1")
+        clicks++;
+        string cardTag = gameObject.tag;
+
+        if (cardTag == "Card1") // checks if card 1 clicked first
+        {
+            gameObject.SetActive(false);    // hides card 1 backside
+            if (clicks % 2 == 0)
+            {
+                string frontCardTag = cardFrontList[0];
+
+                GameObject[] frontCardMatches = GameObject.FindGameObjectsWithTag(frontCardTag);
+
+                foreach (GameObject card in frontCardMatches)
+                {
+                    if (card.activeSelf)
+                    {
+                        Destroy(card);
+                        Destroy(gameObject);
+                    }
+                }
+            }
+
+        }
+
+        if (cardTag == "Card2")
+        {   
+            gameObject.SetActive(false);
+
+        }
+        if (cardTag == "Card3")
         {
 
         }
-        if (objectName == "Card_BackSide2")
+        if (cardTag == "Card4")
         {
 
         }
-        if (objectName == "Card_BackSide3")
+        if (cardTag == "Card5")
         {
 
         }
-        if (objectName == "Card_BackSide4")
-        {
-
-        }
-        if (objectName == "Card_BackSide5")
-        {
-
-        }
-        if (objectName == "Card_BackSide6")
+        if (cardTag == "Card6")
         {
             
         }
-        Debug.Log(gameObject.name + " was clicked!");
-        Destroy(gameObject);
+        
     }
 }
