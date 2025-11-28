3750fa3dffdc54f3280c06e0e9f733a5ccdea511
rory <victoria.harvey1@icloud.com>
2025-11-27 17:05:35 -0800

    updated ui

--- FinalProjectCS276/Assets/Scripts/Menu.cs
+++ FinalProjectCS276/Assets/Scripts/Menu.cs
@@ -6,15 +6,25 @@ using System.Collections;
 
 public class Menu : MonoBehaviour
 {
+
+    public UIDocument uiDocument;
+    private Button level1Button;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        level1Button = uiDocument.rootVisualElement.Q<Button>("Level1Button");
+        level1Button.style.display = DisplayStyle.Flex;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        level1Button.clicked += Level1Scene;
+    }
+
+    void Level1Scene()
+    {
+        SceneManager.LoadScene("Game");
     }
 }
